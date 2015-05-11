9add573b1b53a6d3fe1dcb13a6152dea1a04b33f
Kelli Muhindi <muhindi09@gmail.com>
2015-05-11 21:40:10 +0300

    Third commit; added the project descriptor file

--- file1.c
+++ file1.c
@@ -1,7 +1,12 @@
+/**
+ * This is the original file1.c used to create the 
+ * project
+ * */
 # include <stdio.h>
 
 int main(){
 	printf("This is file 1");
+	printf("This is a test! ");
 	return 0;
 }
 
